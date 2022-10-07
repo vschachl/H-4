2d63666b40d0588987464d56dfd2544384ee3b41
vschachl <valentina.schachl@icloud.com>
2022-10-07 11:00:02 +0200

    commit before ois kaputt mochn dua

--- src/net/htlgkr/schachlv190241/hue4/Aufgabe1.java
+++ src/net/htlgkr/schachlv190241/hue4/Aufgabe1.java
@@ -8,17 +8,62 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.concurrent.Executors.newFixedThreadPool;
 
 public class Aufgabe1 {
     List<Integer> numbers = new ArrayList<>();
+    Stream<List<Integer>> chunks = null;
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
         Aufgabe1 aufgabe1 = new Aufgabe1();
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) newFixedThreadPool(2);
+
+        int divider = -1;
+        int chunk = -1;
 
         //readCSV
         aufgabe1.readCSV();
 
-        aufgabe1.numbers.forEach(s -> System.out.println(s));
+        //checke ob teilbar durch Benutzereingabe
+        System.out.println("Wie lange soll ein Chunk sein?");
+        chunk = Integer.parseInt(s.nextLine());
+        try {
+            aufgabe1.chunks = aufgabe1.batches(aufgabe1.numbers, chunk);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Geben Sie einen beliebigen Teiler ein:");
+        divider = Integer.parseInt(s.nextLine());
+        for (int i = 0; i <= 2; i++){
+
+        }
+        aufgabe1.checkDivider(divider);
+    }
+    public void checkDivider(int divider){
+        for (int i = 0; i < numbers.size(); i++){
+            if (numbers.get(i)%divider == 0){
+                System.out.println(numbers.get(i));
+            }
+        }
+    }
+
+    public Stream<List<Integer>> batches(List<Integer> source, int length) throws IllegalAccessException {
+        if(length <= 0){
+            throw new IllegalAccessException("length = " + length);
+        }
+        int size = source.size();
+        if(size <= 0){
+            return Stream.empty();
+        }
+        int fullChunks = (size -1) /length;
+        return IntStream.range(0, fullChunks+1)
+                .mapToObj(n -> source.subList(n*length, n == fullChunks ? size : (n+1)*length));
     }
 
     public void readCSV(){
@@ -28,17 +73,14 @@ public class Aufgabe1 {
             while (scanner.hasNextLine()){
                 String read = scanner.nextLine();
                 String[] split = read.split(":");
-                String alphabet = "abcdefghijklmnopqrstuvwxyz";
 
                 for (int i = 0; i < split.length; i++){
 
                     if (split[i] != null && !split[i].equals("")&& split[i].matches("[+-]?\\d*(\\.\\d+)?")){
-
                         numbers.add(Integer.valueOf(split[i]));
                     }
                 }
             }
-
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
---
+++ src/net/htlgkr/schachlv190241/hue4/Teilen.java
@@ -0,0 +1,13 @@
+package net.htlgkr.schachlv190241.hue4;
+
+public class Teilen implements Runnable{
+private int divider;
+private int chunk;
+
+
+
+    @Override
+    public void run() {
+
+    }
+}
